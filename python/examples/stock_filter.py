5a66f5e023d1b3fc3c6827cbfb093da8fb237354
vikram <patilvikram@gmail.com>
2017-04-27 16:14:23 +0530

    Added an example to filter stock ticker based on value

---
+++ python/examples/stock_filter.py
@@ -0,0 +1,14 @@
+
+
+def f(a):
+  input_data=a.split(',')
+  if float(input_data[2])> 100:
+     return True
+  return False
+
+from pyapex import createApp
+a=createApp('python_app').fromKafka08('localhost:2181','test_topic')
+  \.setFilter('filter_operator',f)
+  \.toConsole(name='endConsole')
+  \.launch(False)
+
